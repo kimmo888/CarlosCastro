e88e27dee6e7de0318ac0149cdc3af9866fc121e
Kastro888 <kasto777333@gmail.com>
2022-08-09 12:54:57 -0500

    AboutMy

---
+++ src/pages/AboutMy.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/AboutMy.scss';
+
+const AboutMy = () => {
+    return (
+        <div className='ContenedorAboutMy'>
+            <h1>About My</h1>
+        </div>
+    )
+}
+
+export default AboutMy;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import FrontPage from './FrontPage';
 import Project from './Project';
 import Technologies from './Technologies';
 import '../styles/Home.scss';
+import AboutMy from './AboutMy';
 
 const Home = () => {
     return (
         <div className='ContainerHome'>
             <Background />
             <FrontPage />
+            <AboutMy />
             <Technologies />
             <Project />
         </div>
